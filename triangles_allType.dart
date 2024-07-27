63abd1aec7bb126762960825ebdee45d9491c1de
UsmanHaider77 <musmanhaider77@gmail.com>
2024-07-27 09:55:26 +0500

    All * Pattern With Nested Loop

--- triangles_allType.dart
+++ triangles_allType.dart
@@ -36,16 +36,47 @@
 // }
 
 //*************************************(Triangle Of (Center * ))***************************//
-void main(){
+// void main(){
 
-    for(int i = 0 ; i< 7; i++){
-      var stars='';
-        for(int j = (7-i); j > 1 ;j--) {
-           stars += ' ';
-        }
-      for(int j = 0; j <= i ;j++){
-           stars += '* ';
+//     for(int i = 0 ; i< 7; i++){
+//       var stars='';
+//         for(int j = (7-i); j > 1 ;j--) {
+//            stars += ' ';
+//         }
+//       for(int j = 0; j <= i ;j++){
+//            stars += '* ';
+//     }
+//       print(stars);
+// }
+// }
+
+//*************************************(Triangle Of (Center * ))***************************//
+// import 'dart:io';
+
+// void main() {
+//   var rows = 5;
+//   var a = " ";
+//   for (var i = 1; i < rows; i++) {
+//     stdout.write("${a * (rows - i)}");
+//     for (var j = 1; j <= i; j++) {
+//       stdout.write("* ");
+//     }
+//     print("");
+//   }
+// }
+
+//*************************************(Triangle Of (Center 1 To 10 Numbers ))***************************//
+import 'dart:io';
+
+void main() {
+  var rows = 5;
+  var counter = 1;
+  var a = " ";
+  for (var i = 1; i < rows; i++) {
+    stdout.write("${a * (rows - i)}");
+    for (var j = 1; j <= i; j++) {
+      stdout.write("${counter++} ");
     }
-      print(stars);
-}
+    print("");
+  }
 }
